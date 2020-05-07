60bed6b86c0eb27b6db8a859830f41ac072f077a
Noel E. Valentin Roman <noel.valentin2@upr.edu>
2020-05-07 13:27:33 -0400

    Added doubleword capabilities to RAM
    Updated RAM so that it handles double words

--- Ram_Updated.v
+++ Ram_Updated.v
@@ -2,7 +2,7 @@ module ram512x8 (output reg MOC, output reg [31:0] DataOut, input
 MOV, ReadWrite, input [31:0] Address, input [31:0]
 DataIn, input [1:0] datatype); //se incluyo datatype para determinar el tamano del dato 
 
-//00: byte(8 bits)   01: half-word(16 bits)   10: word(32 bits)
+	//00: byte(8 bits)   01: half-word(16 bits)   10: word(32 bits)  11: doubleword (64 bits)
 
 	reg [7:0] Mem[0:511];    //512 localizaciones de 1 byte
 	initial MOC = 1'b0;      //Memory Operation Complete comienza como cero.
@@ -43,6 +43,19 @@ DataIn, input [1:0] datatype); //se incluyo datatype para determinar el tamano d
 						DataOut[7:0] = Mem[Address + 3];
 						#1 MOC = 1'b1;
 					end
+					2'b11:            //11:doubleword
+				        begin 
+						DataOut[31:24] = Mem[Address];
+						DataOut[23:16] = Mem[Address+1];
+                  				DataOut[15:8] = Mem[Address+2];
+						DataOut[7:0] = Mem[Address+3];
+                    				#2                                   //delay para darle tiempo al data bus
+                    				DataOut[31:24] = Mem[Address+4];
+						DataOut[23:16] = Mem[Address+5];
+                    				DataOut[15:8] = Mem[Address+6];
+                    				DataOut[7:0] = Mem[Address+7];
+                    				#1 MOC = 1'b1;
+					end
 					
 				endcase
 				
@@ -73,6 +86,20 @@ DataIn, input [1:0] datatype); //se incluyo datatype para determinar el tamano d
 						Mem[Address + 2] = DataIn[15:8];
 						Mem[Address + 3] = DataIn[7:0];
 						#1 MOC = 1'b1;
+					end
+					
+					2'b11:           //11: DoubleWord 
+					begin 
+						Mem[Address] = DataIn[31:24];
+                    				Mem[Address+1] = DataIn[23:16];
+                    				Mem[Address+2] = DataIn[15:8];
+                    				Mem[Address+3] = DataIn[7:0];
+                    				#2
+                    				Mem[Address+4] = DataIn[31:24];
+                    				Mem[Address+5] = DataIn[23:16];
+                    				Mem[Address+6] = DataIn[15:8];
+                    				Mem[Address+7] = DataIn[7:0];
+                    				#1 MOC = 1'b1;
 					end
 					
 				endcase 
